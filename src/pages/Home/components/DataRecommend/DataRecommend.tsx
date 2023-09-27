4fe54d3d86be42c5613422533e4d61d63ba3289d
Kris0707 <francokris005@gmail.com>
2023-09-27 14:50:07 -0300

    containerRecomend-ResponsiveTerminar

--- src/pages/Home/components/DataRecommend/DataRecommend.tsx
+++ src/pages/Home/components/DataRecommend/DataRecommend.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-confusing-void-expression */
 import { DataPort_Item } from "@/interface";
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { CardRecomend } from "./CardRecomend";
 import { PiArrowCircleLeftFill, PiArrowCircleRightFill } from "react-icons/pi";
 
@@ -10,13 +10,21 @@ interface DataRecommendProps {
 }
 
 const DataRecommend: React.FC<DataRecommendProps> = ({ DataPorts }) => {
-
+  // const [indexRecomend, setIndexRecomend] = useState<number>(0);
+  // const [maxIndex, setMaxIndex] = useState<number>(0);
+  
+  // const [cardsNumbers, setCardNumbers] = useState<number>(0)
+  
+  //Recorre el indice segun el numero del estado local
   const [indexRecomend, setIndexRecomend] = useState(0);
 
-  const maxIndex = DataPorts.length - 4
-  const DataSlice = DataPorts.slice(indexRecomend, indexRecomend + 4)
 
-  const handleNextClick = ():void => {
+  //Para desabilitar el boton del paginado del lado derecho.
+  const maxIndex = DataPorts.length - 4
+
+  const DataSlice = DataPorts.slice(indexRecomend, indexRecomend + 4)
+  
+const handleNextClick = ():void => {
 	if(indexRecomend !== maxIndex) setIndexRecomend(prevIndex => prevIndex + 1);
   };
 
@@ -28,8 +36,9 @@ const DataRecommend: React.FC<DataRecommendProps> = ({ DataPorts }) => {
   return (
     <DataRecommendStl>
       <PiArrowCircleLeftFill
-        size={"2.5rem"}
-        className={`${indexRecomend === 0 ? "arrow-left-disable" : "arrow-page-1"}`}
+        size="3em"
+        style={{}}
+        className={`${indexRecomend === 0 ? "arrow-left-disable" : "arrow-left"}`}
         onClick={handlePrevClick}
       />
       {DataSlice.map((item) => (
@@ -41,14 +50,32 @@ const DataRecommend: React.FC<DataRecommendProps> = ({ DataPorts }) => {
         />
       ))}
       <PiArrowCircleRightFill
-        size={"2.5rem"}
-		className={`${indexRecomend === maxIndex ? "arrow-right-disable" : "arrow-page-2"}`}
+        size="3em"
+		className={`${indexRecomend === maxIndex ? "arrow-right-disable" : "arrow-right"}`}
         onClick={handleNextClick}
       />
     </DataRecommendStl>
   );
 };
 
+
+
+
+const arrowFade = keyframes`
+  0% {
+    opacity: 0;
+    
+  }
+  /* 50%{
+	opacity: 0.5;
+  } */
+  100% {
+    opacity: 1;
+  }
+  0%,100% {
+    transition: cubic-bezier(0.35,-0.01, 0.58, 1)
+  }
+`;
 const DataRecommendStl = styled.div`
   overflow: hidden;
 
@@ -57,36 +84,40 @@ const DataRecommendStl = styled.div`
   align-items: center;
   border-radius: 10px;
 
-  width: 100%;
+  width: 95%;
   height: fit-content;
   
   
-  transition: 0.3s ease-in-out;
 
-
-  .arrow-page-1 {
-    margin: 0 10px 0 0;
+  .arrow-left {
+    /* margin: 0 10px 0 0; */
+    animation: ${arrowFade} 0.3s ease-in-out;
   }
-  .arrow-page-2 {
-    margin: 0 0 0 10px;
+  .arrow-right {
+    /* margin: 0 10px 0 10px; */
+    animation: ${arrowFade} 0.3s ease-in-out;
   }
-  .arrow-page-1,
-  .arrow-page-2 {
+  .arrow-left,
+  .arrow-right {
     cursor: pointer;
   }
 
   .arrow-left-disable {
-	margin: 0 10px 0 0;
+	/* margin: 0 10px 0 0; */
 	opacity: 0.4;
   }
 
   .arrow-right-disable {
-	margin: 0 0 0 10px;
+	/* margin: 0 0 0 10px; */
 	opacity: 0.4;
   }
 
   @media screen and (max-width: 1640px) {
-    width: 100%;
+    /* .arrow-left-disable,.arrow-left {
+      margin: 0 15px 0 0;
+    } */
+
+    width: 90%;
   }
 
   @media screen and (max-width: 1440px) {
